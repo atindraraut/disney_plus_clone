7bd784cfe36900d9ed69edf5bf8b3f7cfd136de9
atindraraut <atindraraut80.com>
2022-09-12 22:54:33 +0530

    Done header ui component

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Slider from "react-slick";
+import "~slick-carousel/slick/slick.css"; 
+import "~slick-carousel/slick/slick-theme.css";
+import styled from 'styled-components';
+
+function ImgSlider() {
+  return (
+    <Carousel>
+      
+    </Carousel>
+  )
+}
+
+export default ImgSlider
+const Carousel=styled(Slider)`
+
+`
