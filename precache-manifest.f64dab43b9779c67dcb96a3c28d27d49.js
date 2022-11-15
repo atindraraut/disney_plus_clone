c9f73e5f3f2777d879ad7174705cdfbe1ca4f590
atindraraut <atindraraut80@gmail.com>
2022-11-15 09:39:14 +0530

    Updates

---
+++ precache-manifest.f64dab43b9779c67dcb96a3c28d27d49.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a8a4c8261850efaf1d2",
+    "url": "/disney_plus_clone/static/css/main.1e5f8d24.chunk.css"
+  },
+  {
+    "revision": "8a8a4c8261850efaf1d2",
+    "url": "/disney_plus_clone/static/js/main.ef442a03.chunk.js"
+  },
+  {
+    "revision": "a4d5fff966eb9f6d279d",
+    "url": "/disney_plus_clone/static/js/runtime~main.52df09a9.js"
+  },
+  {
+    "revision": "447a0c52a104c899fa3a",
+    "url": "/disney_plus_clone/static/css/2.3df6e7fd.chunk.css"
+  },
+  {
+    "revision": "447a0c52a104c899fa3a",
+    "url": "/disney_plus_clone/static/js/2.c827ffbd.chunk.js"
+  },
+  {
+    "revision": "664bc1d84169ff29bca9",
+    "url": "/disney_plus_clone/static/js/3.801fde01.chunk.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/disney_plus_clone/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/disney_plus_clone/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/disney_plus_clone/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/disney_plus_clone/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "70b40e7b00209103c49018c64fb564d7",
+    "url": "/disney_plus_clone/index.html"
+  }
+];
