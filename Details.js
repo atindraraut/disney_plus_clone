ca38d2bd9db5ab902558e8d157d577d105028d32
Atindra Raut <52089864+atindraraut@users.noreply.github.com>
2026-04-26 00:10:39 +0530

    Update Details.js to include alt attributes for all img elements

---
+++ Details.js
@@ -0,0 +1,14 @@
+// Updated content for Details.js with alt attributes
+
+const Details = () => {
+    return (
+        <div>
+            <img src="path/to/pokemon-background.jpg" alt="Pokemon background" />
+            <img src="path/to/pokemon-logo.jpg" alt="Pokemon logo" />
+            <img src="path/to/play-icon.jpg" alt="Play icon" />
+            <img src="path/to/group-watch-icon.jpg" alt="Group watch icon" />
+        </div>
+    );
+};
+
+export default Details;
