3696e376e91916511ce42192402bbc8ff5e8d617
Atindra Raut <52089864+atindraraut@users.noreply.github.com>
2026-04-26 00:11:36 +0530

    Update Viewers.js with alt attributes for viewer images

---
+++ Viewers.js
@@ -0,0 +1,11 @@
+// Updated Viewers.js to include alt attributes for images
+
+const viewers = [
+    { src: 'path/to/disney.jpg', alt: 'Disney' },
+    { src: 'path/to/pixar.jpg', alt: 'Pixar' },
+    { src: 'path/to/marvel.jpg', alt: 'Marvel' },
+    { src: 'path/to/starwars.jpg', alt: 'Star Wars' },
+    { src: 'path/to/nationalgeographic.jpg', alt: 'National Geographic' }
+];
+
+export default viewers;
